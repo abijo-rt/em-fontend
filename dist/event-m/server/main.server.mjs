002b8bfa00d63355d90c3333653b7f6a83bcabf8
abijo-rt <abijorajesh@gmail.com>
2024-10-04 21:03:32 +0530

    delpy3

--- dist/event-m/server/main.server.mjs
+++ dist/event-m/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7XKFHTA6.mjs";import"./chunk-TGBD22H7.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-GGZCKTDN.mjs";import"./chunk-TGBD22H7.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- environment.prod.ts
+++ environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    URL: 'https://em-backend-1.onrender.com'
+    URL: 'https://em-backend-1.onrender.com/'
   };
   
